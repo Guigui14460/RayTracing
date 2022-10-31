4bc3ece5d2247d5796ea20e8bbf9484f3deaa7c6
Guigui14460 <letgui2@gmail.com>
2022-10-31 14:24:45 +0100

    episode 9

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -59,7 +59,7 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y) {
 		Renderer::HitPayload payload = TraceRay(ray);
 
 		if (payload.HitDistance < 0.0f) {
-			glm::vec3 skyColor(0.0f);
+			glm::vec3 skyColor(0.6f, 0.7f, 0.9f);
 			color += skyColor * multiplier;
 			break;
 		}
@@ -68,14 +68,15 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y) {
 		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.f); // == cos(angle)
 
 		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
-		glm::vec3 sphereColor = sphere.Albedo;
+		const Material& sphereMaterial = m_ActiveScene->Materials[sphere.MaterialIndex];
+		glm::vec3 sphereColor = sphereMaterial.Albedo;
 		sphereColor *= lightIntensity;
 
 		color += sphereColor * multiplier;
 		multiplier *= 0.7f;
 
 		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
-		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal + sphereMaterial.Roughness * Walnut::Random::Vec3(-0.5f, 0.5f));
 	}
 
 	return glm::vec4(color, 1);
